1eeedcd21ee803ed7258ab141188adf252582252
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2025-10-01 13:34:22 +0200

    feat: Fix template literals at constants.mjs

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -1,10 +1,10 @@
 const API_BASE_URL = "https://v2.api.noroff.dev";
 
 export const API_KEY = import.meta.env.VITE_API_KEY;
-export const REGISTER_ENDPOINT = `${API_BASE_URL + "/auth/register"}`;
-export const LOGIN_ENDPOINT = `${API_BASE_URL + "/auth/login"}`;
-export const AUCTION_LISTING_ENDPOINT = `${API_BASE_URL + "/auction/listings"}`;
-export const AUCTION_PROFILES_ENDPOINT = `${API_BASE_URL + "/auction/profiles"}`;
+export const REGISTER_ENDPOINT = `${API_BASE_URL}/auth/register`;
+export const LOGIN_ENDPOINT = `${API_BASE_URL}/auth/login`;
+export const AUCTION_LISTING_ENDPOINT = `${API_BASE_URL}/auction/listings`;
+export const AUCTION_PROFILES_ENDPOINT = `${API_BASE_URL}/auction/profiles`;
 
 /**
  * Generates dynamic API endpoints based on the logged-in user's name.
@@ -25,14 +25,10 @@ export function getAuctionEndpoints() {
 
   if (userName) {
     return {
-      AUCTION_SINGLE_PROFILE: `${AUCTION_PROFILES_ENDPOINT + "/" + userName.name}`,
-      AUCTION_SINGLE_LISTINGS: `${
-        AUCTION_PROFILES_ENDPOINT + "/" + userName.name + "/listings?_seller=true&_bids=true"
-      }`,
-      AUCTION_SINGLE_WINS: `${
-        AUCTION_PROFILES_ENDPOINT + "/" + userName.name + "/wins?_seller=true&_bids=true&_listings=true"
-      }`,
-      AUCTION_SINGLE_BIDS: `${AUCTION_PROFILES_ENDPOINT + "/" + userName.name + "/bids?_listings=true&_bids=true"}`,
+      AUCTION_SINGLE_PROFILE: `${AUCTION_PROFILES_ENDPOINT}/${userName.name}`,
+      AUCTION_SINGLE_LISTINGS: `${AUCTION_PROFILES_ENDPOINT}/${userName.name}/listings?_seller=true&_bids=true`,
+      AUCTION_SINGLE_WINS: `${AUCTION_PROFILES_ENDPOINT}/${userName.name}/wins?_seller=true&_bids=true&_listings=true`,
+      AUCTION_SINGLE_BIDS: `${AUCTION_PROFILES_ENDPOINT}/${userName.name}/bids?_listings=true&_bids=true`,
     };
   } else {
     return {};
